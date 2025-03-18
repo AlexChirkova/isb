120af14fe6046e393e17e2c1e92ef8f7d5865426
Sasha Sherwood <leksachirkova@gmail.com>
2025-03-18 23:46:14 +0400

    Added file with constants

---
+++ Lab1/constants.py
@@ -0,0 +1,11 @@
+ALPHABET = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ "
+
+INPUT_FILE_1 = "message.txt"
+OUTPUT_FILE_1 = "encoded_message.txt"
+
+INPUT_FILE_2 = "text.txt"
+OUTPUT_FILE_2 = "decoded_text.txt"
+CODE_ALPHABET = "code_alph2.txt"
+KEY = "key.json"
+
+
