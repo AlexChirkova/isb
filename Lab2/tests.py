03e2029b63c2caed4e834d6afe47f3ba46f0ae3f
Sasha Sherwood <leksachirkova@gmail.com>
2025-04-16 12:54:47 +0400

    Created 3 tests

---
+++ Lab2/tests.py
@@ -0,0 +1,60 @@
+import math
+
+from scipy.special import gammainc
+
+from Random import random_sequence as rs
+from constants import pi
+
+
+def frequency_bitwise_test(seq: str) -> float:
+    """
+    Frequency bitwise test
+    :param seq: bitwise sequence for test
+    :return: the probability that the value is close to the benchmark
+    """
+
+    n = len(seq)
+    s = abs(1/(math.sqrt(n))*(seq.count('1') - seq.count('0')))
+    return math.erfc(s/math.sqrt(2))
+
+
+def consecutive_bits_test(seq: str) -> float:
+    """
+    A test for identical consecutive bits
+    :param seq: bitwise sequence for test
+    :return: the probability that the value is close to the benchmark
+    """
+    n = len(seq)
+    p = seq.count('1')/n
+    if abs(p - 0.5) >= 2/math.sqrt(n): return 0
+
+    v = sum(seq[i] != seq[i + 1] for i in range(n - 1))
+    return math.erfc(abs(v - 2*n*p*(1-p))/(2*math.sqrt(2*n)*p*(1-p)))
+
+
+def longest_seq_of_units_test(seq: str) -> float:
+    """
+    Test for the longest sequence of units in the block
+    :param seq: bitwise sequence for test
+    :return: the probability that the value is close to the benchmark
+    """
+    n = len(seq)
+    v = [0, 0, 0, 0]
+    for i in range(0, n, 8):
+        max_len = 0
+        current_len = 0
+        for j in seq[i:i+8+1]:
+            if j == '1':
+                current_len += 1
+                max_len = max(max_len, current_len)
+            else: current_len = 0
+
+        match max_len:
+            case 0: v[0] += 1
+            case 1: v[0] += 1
+            case 2: v[1] += 1
+            case 3: v[2] += 1
+            case _: v[3] += 1
+
+    he2 = sum(((v[i] - 16*pi[i])**2)/16*pi[i] for i in range(4))
+    return gammainc(3/2, he2/2)
