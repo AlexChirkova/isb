74a90c1b25bb099371dc968b4fb3dab3f96a2b38
Sasha Sherwood <leksachirkova@gmail.com>
2025-05-14 13:08:28 +0400

    Constants

---
+++ Lab3/constants.py
@@ -0,0 +1,6 @@
+PATH_TO_PLAINTEXT = "texts/plaintext.txt"
+PATH_TO_CIPHERTEXT = "texts/ciphertext.txt"
+
+PATH_TO_SYM_KEY = "keys/symmetric_key.txt"
+PATH_TO_PRIVATE_KEY = "keys/private_key.txt"
+PATH_TO_PUBLIC_KEY = "keys/public_key.txt"
