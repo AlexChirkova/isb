14f707453e4fd0be359567a79665544dd07cd1b5
Sasha Sherwood <leksachirkova@gmail.com>
2025-06-10 01:07:27 +0400

    A module for determining the time dependence on the number of processes

---
+++ Lab4/time_measurer.py
@@ -0,0 +1,30 @@
+import time
+import multiprocessing
+
+
+from card_finder import CardFinder
+
+
+class TimeMeasurer:
+    def __init__(self):
+        self.cpu_count = multiprocessing.cpu_count()
+
+    def measure_performance(self, card_hash, bin_prefixes, last4_digits):
+        """
+        Measure performance with different numbers of processes
+        :param card_hash: hash value
+        :param bin_prefixes: list of BINs
+        :param last4_digits: last 4 digits
+        :return: Table of time dependence on the number of processes
+        """
+        max_processes = int(1.5 * self.cpu_count)
+        results = []
+
+        for num_processes in range(1, max_processes + 1):
+            start_time = time.time()
+            card = CardFinder()
+            card.find_card_number(bin_prefixes, last4_digits, card_hash)
+            elapsed = time.time() - start_time
+            results.append((num_processes, elapsed))
+
+        return results
